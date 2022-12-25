9b870d17d3f6dfc9d56a5b76172f77f3ac915f08
harrykamboj1 <singhharnoor116@gmail.com>
2022-12-25 20:31:59 +0530

    Subset With Duplicate Elements

--- src/com/dsAlgo/Recursion/Permutation.java
+++ src/com/dsAlgo/Recursion/Permutation.java
@@ -1,4 +1,36 @@
 package com.dsAlgo.Recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Permutation {
+    public static void main(String[] args) {
+          int[] nums = {1,2,2};
+
+        System.out.println(subsetsWithDup(nums));
+    }
+
+    // 1. Subset With Duplicate
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> outerList = new ArrayList<>();
+        List<Integer> innerList = new ArrayList<>();
+        makeSubset(nums,0,outerList,innerList);
+        return outerList;
+    }
+
+    private static void makeSubset(int[] nums, int index, List<List<Integer>> outerList, List<Integer> innerList) {
+       outerList.add(innerList);
+
+       for(int i = index;i<nums.length;i++){
+           if(i>index && nums[i] == nums[i-1]){
+               continue;
+           }
+           List<Integer> p = new ArrayList<>(innerList);
+           p.add(nums[i]);
+           makeSubset(nums,i+1,outerList,p);
+       }
+
+    }
 }
