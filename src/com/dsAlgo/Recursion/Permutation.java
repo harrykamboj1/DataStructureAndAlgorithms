9c5f6c86012dbaa20d4c0a5484534d7b92aa3f3c
harrykamboj1 <singhharnoor116@gmail.com>
2022-12-26 08:15:34 +0530

    Permutation of given numbers in array using recursion

--- src/com/dsAlgo/Recursion/Permutation.java
+++ src/com/dsAlgo/Recursion/Permutation.java
@@ -1,36 +1,37 @@
 package com.dsAlgo.Recursion;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Permutation {
     public static void main(String[] args) {
-          int[] nums = {1,2,2};
-
-        System.out.println(subsetsWithDup(nums));
+        int[] nums = {1,2,3};
+        System.out.println(permute(nums));
     }
 
-    // 1. Subset With Duplicate
-    public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> outerList = new ArrayList<>();
-        List<Integer> innerList = new ArrayList<>();
-        makeSubset(nums,0,outerList,innerList);
-        return outerList;
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> dataStructure = new ArrayList<>();
+        boolean[] freq  = new boolean[nums.length];
+
+        permutateAnswer(nums,dataStructure,answer,freq);
+        return answer;
     }
 
-    private static void makeSubset(int[] nums, int index, List<List<Integer>> outerList, List<Integer> innerList) {
-       outerList.add(innerList);
-
-       for(int i = index;i<nums.length;i++){
-           if(i>index && nums[i] == nums[i-1]){
-               continue;
-           }
-           List<Integer> p = new ArrayList<>(innerList);
-           p.add(nums[i]);
-           makeSubset(nums,i+1,outerList,p);
-       }
-
+    private static void permutateAnswer(int[] nums, List<Integer> dataStructure, List<List<Integer>> answer, boolean[] freq) {
+          if(dataStructure.size() == nums.length){
+              answer.add(new ArrayList<>(dataStructure));
+              return;
+          }
+        for (int i = 0; i < nums.length; i++) {
+            if(!freq[i]){
+                dataStructure.add(nums[i]);
+                freq[i] = true;
+                permutateAnswer(nums,dataStructure,answer,freq);
+                // After using the given number we delete it from dataStructure array.
+                dataStructure.remove(dataStructure.size()-1);
+                freq[i] = false;
+            }
+        }
     }
 }
