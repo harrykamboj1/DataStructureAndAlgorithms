82d55df98b36839a9c18c5ffcba8e6c79cb4a863
harrykamboj1 <singhharnoor116@gmail.com>
2022-12-28 07:55:23 +0530

    Maze Problem Print Path and Store in array

--- src/com/dsAlgo/Recursion/MazeProblem.java
+++ src/com/dsAlgo/Recursion/MazeProblem.java
@@ -1,8 +1,10 @@
 package com.dsAlgo.Recursion;
 
+import java.util.ArrayList;
+
 public class MazeProblem {
     public static void main(String[] args) {
-        path("",3,3);
+        System.out.println(pathRet("",3,3));
     }
 
 
@@ -30,5 +32,24 @@ public class MazeProblem {
         if(c>1){
             path(p+'R',r,c-1);
         }
+    }
+
+    public static ArrayList<String> pathRet(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> newList = new ArrayList<>();
+            newList.add(p);
+            return newList;
+        }
+
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if(r>1){
+            list.addAll(pathRet(p+'D',r-1,c));
+        }
+        if(c>1){
+            list.addAll(pathRet(p+'R',r,c-1));
+        }
+        return list;
     }
 }
