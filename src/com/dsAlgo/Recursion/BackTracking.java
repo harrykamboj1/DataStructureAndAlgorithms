bbc59a381e3cfaa7590b58cbf62f5e6de131f721
harrykamboj1 <singhharnoor116@gmail.com>
2022-12-29 07:53:33 +0530

    BackTracking Print

--- src/com/dsAlgo/Recursion/BackTracking.java
+++ src/com/dsAlgo/Recursion/BackTracking.java
@@ -1,5 +1,7 @@
 package com.dsAlgo.Recursion;
 
+import java.util.Arrays;
+
 public class BackTracking {
     public static void main(String[] args) {
         boolean[][] board = {
@@ -7,9 +9,52 @@ public class BackTracking {
                 {true,true,true},
                 {true,true,true}
         };
-
-        backTracking("",board,0,0);
+        int[][] path = new int[board.length][board[0].length];
+        backTrackingPrint("",board,0,0,path,1);
     }
+
+    private static void backTrackingPrint(String p, boolean[][] maze, int row, int col, int[][] path, int step) {
+        if(row == maze.length-1 && col == maze[0].length-1){
+            for(int[] arr :path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        //To check condition whether any path is blocked or not
+        if(!maze[row][col]){
+            return;
+        }
+
+
+        // while going for recursive calls mark the path as false as they are already visited
+        maze[row][col] = false;
+        path[row][col] = step;
+
+        if(row<maze.length-1){
+            backTrackingPrint(p+"D",maze,row+1,col,path,step+1);
+        }
+
+        if(col<maze[0].length-1){
+            backTrackingPrint(p+"R",maze,row,col+1,path,step+1);
+        }
+
+        if(row>0){
+            backTrackingPrint(p+"U",maze,row-1,col,path,step+1);
+        }
+        if(col>0){
+            backTrackingPrint(p+"L",maze,row,col-1,path,step+1);
+        }
+
+        //this is the line where fxn will over
+        //so before fxn gets removed ,also remove the changes that were made by fxn
+        //that is making path not visited again
+        maze[row][col] = true;
+        path[row][col] = 0;
+    }
+
 
     private static void backTracking(String p, boolean[][] maze, int row, int col) {
 
