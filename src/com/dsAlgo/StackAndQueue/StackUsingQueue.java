644bc7b14969866217bbb1696926c43cd60f10fe
harrykamboj1 <singhharnoor116@gmail.com>
2023-01-04 08:04:02 +0530

    225. Implement Stack using Queues

---
+++ src/com/dsAlgo/StackAndQueue/StackUsingQueue.java
@@ -0,0 +1,48 @@
+package com.dsAlgo.StackAndQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingQueue {
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(3);
+        s.push(2);
+        s.push(4);
+        s.push(1);
+        System.out.println("Top of the stack: " + s.top());
+        System.out.println("The deleted element is: " + s.pop());
+        System.out.println("The Stack is empty or not : " + s.empty());
+        System.out.println("Top of the stack after removing element: " + s.top());
+    }
+
+}
+
+class Stack {
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    public Stack() {
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.add(queue.peek());
+            queue.remove();
+        }
+    }
+
+    public int pop() {
+        return queue.remove();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
