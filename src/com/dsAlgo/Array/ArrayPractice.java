2ae653506660762a0a2ae1bda587c10ff9e4e973
BhavnaGarg <bhavnagarg12ja>
2023-01-05 22:20:32 +0530

    34. Find First and Last Position of Element in Sorted Array

--- src/com/dsAlgo/Array/ArrayPractice.java
+++
@@ -1,7 +0,0 @@
-package com.dsAlgo.Array;
-
-public class ArrayPractice {
-    public static void main(String[] args) {
-        System.out.println("Hllo World");
-    }
-}
---
+++ src/com/dsAlgo/Array/FirstAndLastPosition.java
@@ -0,0 +1,44 @@
+package com.dsAlgo.Array;
+
+import java.util.Arrays;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args){
+        int[]nums={5,7,7,8,8,10};
+        int target=8;
+        int[] ans= searchRange(nums,target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] searchRange(int[] nums, int target){
+        int[] ans= {-1, -1};
+//     check for the first occurance if target first
+        int start = search(nums, target,true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+//    this function just returns the index value of target
+    static int search(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start=0;
+        int end= nums.length - 1;
+        while (start<=end){
+            int mid= start+ (end-start)/2;
+            if(target< nums[mid]){
+                end = mid - 1;
+            }else if(target > nums[mid]){
+                start = mid + 1;
+            }else{
+// potential ans is found
+                ans = mid;
+                if(findStartIndex){
+                    end= mid-1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
