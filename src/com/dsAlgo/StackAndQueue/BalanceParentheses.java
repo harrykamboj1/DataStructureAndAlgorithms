69f9c761d678fa291b8e9a64f3c05ff45e4f14e5
harrykamboj1 <singhharnoor116@gmail.com>
2023-01-05 08:47:52 +0530

    Check for balanced paranthesis

---
+++ src/com/dsAlgo/StackAndQueue/BalanceParentheses.java
@@ -0,0 +1,27 @@
+package com.dsAlgo.StackAndQueue;
+
+import java.util.Stack;
+
+public class BalanceParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid("(())"));
+    }
+
+
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for (char it : s.toCharArray()) {
+            if (it == '(' || it == '[' || it == '{')
+                st.push(it);
+            else {
+                if(st.isEmpty()) return false;
+                char ch = st.pop();
+                if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.isEmpty();
+
+
+    }
+}
