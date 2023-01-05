5a7f560626a22d84d61625493c8c0d6f724df514
harrykamboj1 <singhharnoor116@gmail.com>
2023-01-05 07:58:14 +0530

    Array changes to ArrayPractice

---
+++ src/com/dsAlgo/Array/ArrayPractice.java
@@ -0,0 +1,7 @@
+package com.dsAlgo.Array;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        System.out.println("Hllo World");
+    }
+}
--- src/com/dsAlgo/Array/arrays.java
+++
@@ -1,9 +0,0 @@
-package com.dsAlgo.Array;
-
-public class arrays {
-    public static void main(String[] args) {
-        System.out.println(
-                "Hllo World"
-        );
-    }
-}
