e7394b44d152b82eb9fae3a425308e21572dcbfc
harrykamboj1 <singhharnoor116@gmail.com>
2023-02-02 07:48:32 +0530

    Pre Order Traversal

---
+++ src/com/dsAlgo/BinaryTree/PreOrderTraversal.java
@@ -0,0 +1,31 @@
+package com.dsAlgo.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreOrderTraversal {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        System.out.println(preorderTraversal(root));
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.add(root.val);
+
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+
+
+        return list;
+    }
+
+
+}
