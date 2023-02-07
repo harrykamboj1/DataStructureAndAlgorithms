b05aecdf9c151111eb682e90b158db60f7166fc8
harrykamboj1 <singhharnoor116@gmail.com>
2023-02-07 21:48:02 +0530

    Striver Sheet

---
+++ src/com/StriverSDEsheet/Array/MatrixZeros.java
@@ -0,0 +1,11 @@
+package com.StriverSDEsheet.Array;
+
+public class MatrixZeros {
+    public static void main(String[] args) {
+
+    }
+
+    public static void setZeroes(int[][] matrix) {
+
+    }
+}
