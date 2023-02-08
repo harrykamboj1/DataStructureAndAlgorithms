7fd0312199646c6ab68d013159ff1baa539bb826
BhavnaGarg <bhavnagarg12ja>
2023-02-08 23:17:50 +0530

    Pascal's Triangle

---
+++ src/com/StriverSDEsheet/Array/PascalTriangle.java
@@ -0,0 +1,27 @@
+package com.StriverSDEsheet.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> row, col=null;
+        for (int i = 0; i < numRows; ++i) {
+            row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(col.get(j - 1) + col.get(j));
+                }
+            }
+                col = row;
+                result.add(row);
+        }
+        return result;
+    }
+}
