f3e15e3932036f831839388322ce839da6782489
harrykamboj1 <singhharnoor116@gmail.com>
2023-02-08 08:03:50 +0530

    Matrix zeros

--- src/com/StriverSDEsheet/Array/MatrixZeros.java
+++ src/com/StriverSDEsheet/Array/MatrixZeros.java
@@ -1,11 +1,36 @@
 package com.StriverSDEsheet.Array;
 
+import java.util.Arrays;
+
 public class MatrixZeros {
     public static void main(String[] args) {
-
+         int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
     }
 
     public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int dummy1[]=new int[rows];
+        int dummy2[]=new int[cols];
+        Arrays.fill(dummy1,-1);
+        Arrays.fill(dummy2,-1);
 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    dummy1[i] = 0;
+                    dummy2[j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dummy1[i] == 0 || dummy2[j]==0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 }
